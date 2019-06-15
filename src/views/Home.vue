c1b2ea58dfab08ca9a5d3fb2a2fd3fafdc1b716e
coltborg <coltborg@gmail.com>
2019-06-15 17:42:16 -0500

    Add github corner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <GithubCorner />
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -8,10 +9,12 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import GithubCorner from "@/components/GithubCorner.vue";
 
 export default {
   name: "home",
   components: {
+    GithubCorner,
     HelloWorld
   }
 };
